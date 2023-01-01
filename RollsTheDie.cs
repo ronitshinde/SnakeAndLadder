f912950bead3e35b5a05ae3125e49c6d2704e06d
ronitshinde <rony.shinde753@gmail.com>
2023-01-01 11:22:59 +0530

    Code to roll the die for first time

---
+++ RollsTheDie.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace SnakeAndLadder
+{
+     public class RollsTheDie
+    {
+        public static void FirstTime()
+        {
+                Random random = new Random();
+                int dice_number = random.Next(1, 7);
+                Console.WriteLine(dice_number);
+        }
+    }
+}
