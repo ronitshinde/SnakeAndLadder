829b70479d8cb8faf4f0596cc6faedb46d861c99
ronitshinde <rony.shinde753@gmail.com>
2023-01-01 10:49:34 +0530

    Start position added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(String[] args) 
+        {
+            int player_no = 1;
+            int start_position = 0;
+            Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no,start_position);
+        }
+    }
+}
