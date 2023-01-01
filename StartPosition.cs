dc897a14c087d0e5f3f8b3f911b5169384c6f349
ronitshinde <rony.shinde753@gmail.com>
2023-01-01 17:00:39 +0530

    Code to Start Position of player

---
+++ StartPosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class StartPosition
+    {
+        public static void PlayerOne()
+        {
+            int player_no = 1;
+            int start_position = 0;
+            Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no, start_position);
+
+        }
+    }
+}
