1870bb07fea79f648d50df7d55ee48e28afeb574
ronitshinde <118701033+ronitshinde@users.noreply.github.com>
2023-01-01 17:20:50 +0530

    Unwanted code removed from RollsTheDice_U

--- Program.cs
+++ Program.cs
@@ -4,13 +4,7 @@
     {
         public static void Main(String[] args) 
         {
-            //int player_no = 1;
-            //int start_position = 0;
-            //Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no,start_position);
-
-            //RollsTheDie.FirstTime();
-            CheckOptions.ToMakeMove();
-        
+            RollsTheDie.FirstTime();         
         }
     }
 }
