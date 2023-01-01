9580789d2972f54282fa7cb8a26083eba5fbbffa
ronitshinde <rony.shinde753@gmail.com>
2023-01-01 12:30:09 +0530

    Check Move method called

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@
     {
         public static void Main(String[] args) 
         {
-            int player_no = 1;
-            int start_position = 0;
-            Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no,start_position);
+            //int player_no = 1;
+            //int start_position = 0;
+            //Console.WriteLine("Start Position of Player no {0} is : {1} ", player_no,start_position);
+
+            //RollsTheDie.FirstTime();
+            CheckOptions.ToMakeMove();
+        
         }
     }
 }
