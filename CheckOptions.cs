1b851b1a8dbdb3e7b5a4f35a32d72d5238f5484e
ronitshinde <rony.shinde753@gmail.com>
2023-01-01 12:28:57 +0530

    Code to check move options

---
+++ CheckOptions.cs
@@ -0,0 +1,38 @@
+﻿
+
+namespace SnakeAndLadder
+{
+    public class CheckOptions
+    {
+        public static void ToMakeMove()
+        {
+            Random random = new Random();
+            int Dice_No = random.Next(1, 7);
+            Console.WriteLine("Number on dice : "+Dice_No);
+
+            int Player_position = 0;
+
+            Random random1 = new Random();
+            int option = random1.Next(1,4);
+            Console.WriteLine("Number of option "+option);
+
+            int No_Play = 0;
+            
+            if (option == 1)        //Moves ahead
+            {
+                Player_position = Player_position + Dice_No;
+                Console.WriteLine("Player position moves ahead by the number on dice : " + Player_position);
+            }
+            else if (option == 2)   //Moves behind
+            {
+                Player_position = Player_position - Dice_No;
+                Console.WriteLine("Player position moves behind by the number on dice : " + Player_position);
+            }
+            else                    //No play
+            {
+                Player_position = Player_position + No_Play;
+                Console.WriteLine("Player position remain same : " + Player_position);
+            }    
+        }
+    }
+}
